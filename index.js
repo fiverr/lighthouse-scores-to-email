1b6925eeba41697387e0c63624b62afce397429a
omrilotan <omri@hamadgera.com>
2020-02-20 17:27:52 +0200

    work in progress I guess?

--- index.js
+++ index.js
@@ -1,44 +1,21 @@
+const { join } = require('path');
 const mailer = require('./lib/mailer');
 const lightHouseApi = require('./lib/lightHouseApi');
-const config = require('./configuration/config.js');
-const fs = require("fs");
+const getCombinations = require('./lib/getCombinations');
+const {
+    categories,
+    strategy: strategies
+} = require('./configuration/config.js');
 
+const pages = require(join(process.cwd(), 'configuration/pages.json'));
 
-
-const categories = config.categories;
-
-function getCombinations(options, optionIndex, results, current) {
-    const allKeys = Object.keys(options);
-    const optionKey = allKeys[optionIndex];
-    const vals = options[optionKey];
-
-    for (let i = 0; i < vals.length; i++) {
-        current[optionKey] = vals[i];
-
-        if (optionIndex + 1 < allKeys.length) {
-            getCombinations(options, optionIndex + 1, results, current);
-        } else {
-            const res = JSON.parse(JSON.stringify(current));
-            results.push(res);
-        }
-    }
-
-    return results;
-}
-
-const strategies = config.strategy;
-const pages = fs.readFileSync('./configuration/pages.json')
-const pagesContent = JSON.parse(pages);
-
-const conf = { strategies, pages: pagesContent };
-
-const PagesCombinations = getCombinations(conf, 0, [], {});
+const PagesCombinations = getCombinations({ strategies, pages }, 0, [], {});
 
 const callApi = async(categories, pagesCombinations) => {
 
     const dataFromApi = [];
 
-    for (let i=0; i < strategies.length; i++)
+    for (let i = 0; i < strategies.length; i++)
     {
         dataFromApi.push(new Array());
     }
@@ -48,7 +25,7 @@ const callApi = async(categories, pagesCombinations) => {
         const pageUrl = Object.values(pagesCombinations[index].pages)[0];
         const strategy = pagesCombinations[index].strategies;
         const pageName = Object.keys(pagesCombinations[index].pages)[0];
-        const resultFromApi = await lightHouseApi.queryApi(pageUrl, strategy, categories, pageName);
+        const resultFromApi = await lightHouseApi(pageUrl, strategy, categories, pageName);
 
         const currectLocation = strategies.findIndex((element) => element === strategy);
         dataFromApi[currectLocation].push(resultFromApi);
@@ -61,7 +38,6 @@ const runNow = async() => {
     const getApiData = await callApi(categories, PagesCombinations);
     await mailer.sendEmail(getApiData);
 };
-
 
 
 runNow();
