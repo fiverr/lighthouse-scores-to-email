3e9d6394ffa994a22a6c4aa2ea0b0cf5c90797f7
omrilotan <omri@hamadgera.com>
2021-06-18 16:37:48 +0100

    this is now a package

--- bin.js
+++ bin.js
@@ -7,32 +7,42 @@ const run = require('.');
 
 start();
 
-process.on('unhandledRejection', ({ message, stack, code, response }) => console.log({
-    level: 'error',
-    message,
-    stack,
-    code,
-    response
-}));
+process.on(
+    'unhandledRejection',
+    ({ message, stack, code, response }) => console.log(
+        JSON.stringify({
+            level: 'error',
+            message,
+            stack,
+            code,
+            response
+        })
+    )
+);
 
-process.on('exit', (code) => logger.info(`About to exit with code ${code}`));
+process.on(
+    'exit',
+    (code) => logger.info(
+        `About to exit with code ${code}`
+    )
+);
 
 async function start() {
     try {
-        let userConfig;
-        try {
-            userConfig = require(join(__dirname, 'configuration/config.json'));
-        } catch (error) {
-            // ignore, this is acceptable
-        }
+        const userConfig = loadUserConfig();
 
         const {
             categories,
-            email,
+            email = {},
             lightHouseApiKey,
             strategies,
-            statsdClient
+            statsdClient = {}
         } = config(userConfig);
+
+        if (!email.to && !statsdClient.host) {
+            logger.warn('No output method was configured (email, statsd). Exit program.');
+            return;
+        }
 
         const pages = require(join(__dirname, 'configuration/pages.json'));
 
@@ -52,5 +62,21 @@ async function start() {
         process.exit(1);
     } finally {
         process.exit();
+    }
+}
+
+/**
+ * Load configuration file if it exists
+ */
+function loadUserConfig() {
+    try {
+        return require(
+            join(
+                __dirname,
+                'configuration/config.json'
+            )
+        );
+    } catch (error) {
+        return undefined;
     }
 }
--- index.js
+++ index.js
@@ -1,22 +1,23 @@
 const pMap = require('p-map');
 const getCombinations = require('./lib/getCombinations');
 const lightHouseApi = require('./lib/lightHouseApi');
-const logger = require('./lib/logger');
+const defaultLogger = require('./lib/logger');
 const sendEmail = require('./lib/sendEmail');
 const sendMetrics = require('./lib/sendMetrics');
 
+/**
+ * @see readme for parameters
+ * @returns {object}
+ */
 module.exports = async function run({
-    categories,
-    email,
+    categories = ['performance', 'seo', 'accessibility', 'best-practices'],
+    strategies = ['mobile', 'desktop'],
+    email = {},
     lightHouseApiKey,
-    strategies,
-    pages,
-    statsdClient
+    pages = {},
+    statsdClient = {},
+    logger = defaultLogger
 }) {
-    if (!email.to && !statsdClient.host) {
-        logger.warn('No output method was configured (email, statsd). Exit program.');
-        return;
-    }
     const strategiesCombinations = getCombinations({ strategies, categories, pages });
     if (strategiesCombinations.flat().length === 0) {
         logger.warn('No pages specified. Exit program.');
@@ -24,38 +25,45 @@ module.exports = async function run({
     }
 
     logger.debug('Retrieve results from PageSpeed API.');
-    const dataFromApi = await pMap(
-        strategiesCombinations,
-        async(strategyCombinations) => await pMap(
-            strategyCombinations,
-            lightHouseApi.bind(null, lightHouseApiKey),
-            { concurrency: 2 }
-        )
-    );
+    try {
+        const dataFromApi = await pMap(
+            strategiesCombinations,
+            async(strategyCombinations) => await pMap(
+                strategyCombinations,
+                lightHouseApi.bind(null, lightHouseApiKey),
+                { concurrency: 2 }
+            )
+        );
 
-    if (email.to) {
-        logger.debug('Send email to recipients.');
-        try {
-            const result = await sendEmail({ email, dataFromApi });
+        if (email.to) {
+            logger.debug('Send email to recipients.');
+            try {
+                const result = await sendEmail({ email, dataFromApi });
 
-            logger.info({
-                message: 'Email sent successfully',
-                response: JSON.stringify(result)
-            });
-        } catch (error) {
-            logger.error(error);
+                logger.info({
+                    message: 'Email sent successfully',
+                    response: JSON.stringify(result)
+                });
+            } catch (error) {
+                logger.error(error);
+            }
         }
-    }
 
-    if (statsdClient.host) {
-        logger.debug('Send metrics to statsd.');
-        try {
-            const { length } = await sendMetrics.send({ statsdClient, dataFromApi });
-            logger.info({
-                message: `${length} metrics were sent successfully.`
-            });
-        } catch (error) {
-            logger.error(error);
+        if (statsdClient.host) {
+            logger.debug('Send metrics to statsd.');
+            try {
+                const { length } = await sendMetrics.send({ statsdClient, dataFromApi });
+                logger.info({
+                    message: `${length} metrics were sent successfully.`
+                });
+            } catch (error) {
+                logger.error(error);
+            }
         }
+
+        return dataFromApi;
+    } catch (error) {
+        logger.error(error);
+        return null;
     }
 };
